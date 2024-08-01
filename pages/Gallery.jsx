a060034531bb42f2b29d0262d0580347c0dddec6
harsha <harshaudayanga401@gmail.com>
2024-08-01 18:59:09 +0530

    play icon

--- pages/Gallery.jsx
+++ pages/Gallery.jsx
@@ -265,67 +265,10 @@ export default function Gallery() {
                                 />
                               
                             </div>
-                            <div className="absolute inset-0 flex items-center justify-center">
-                                <button data-modal-target="default-modal"
-                                    data-modal-toggle="default-modal"
-                                    type="button"
-                                    onClick={toggleModal} className="w-52 h-52 mt-10  p-2 rounded-full shadow-lg"> </button>
-                            </div>
+                            
                         </div>
                     </div>
-                    {/* Main modal */}
-                    {showModal && (
-                        <div
-                            id="default-modal"
-                            tabIndex="-1"
-                            aria-hidden="true"
-                            className="fixed top-0 left-0 right-0 z-50 w-full h-screen bg-black bg-opacity-50 flex justify-center items-center"
-                        >
-                            <div className="relative w-full max-w-xl lg:max-w-5xl bg-white rounded-lg shadow dark:bg-gray-700">
-                                {/* Modal content */}
-                                <div className="relative">
-                                    {/* Modal header */}
-                                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                            Lets Go To Virtual Tour
-                                        </h3>
-                                        <button
-                                            type="button"
-                                            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                            onClick={toggleModal}
-                                        >
-                                            <svg
-                                                className="w-3 h-3"
-                                                aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 14 14"
-                                            >
-                                                <path
-                                                    stroke="currentColor"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                                                />
-                                            </svg>
-                                            <span className="sr-only">Close modal</span>
-                                        </button>
-                                    </div>
-                                    {/* Modal body */}
-                                    <div className="p-6 space-y-6">
-
-                                        <video class="w-screen h-auto max-w-full" controls>
-                                            <source src="/docs/videos/flowbite.mp4" type="video/mp4" />
-                                            Your browser does not support the video tag.
-                                        </video>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    )}
+                  
                 </section>
             </div>
 
