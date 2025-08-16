df9b6827e27871a77cd4c972122856fada6dd9fb
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2025-08-16 21:00:22 +0530

    googletags

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
-import "@/styles/globals.css";
-import 'leaflet/dist/leaflet.css';
+// pages/_app.js
+import Script from "next/script";
+import "../styles/globals.css"; // keep your styles import
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* ✅ Google Ads Global Site Tag */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11405539991"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11405539991');
+        `}
+      </Script>
+
+      {/* ✅ Your App */}
+      <Component {...pageProps} />
+    </>
+  );
 }
