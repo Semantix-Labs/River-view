adf7d616ba68e405076bef94f14c0a8286cb7fc2
shobian <s.shobian@gmail.com>
2026-02-10 05:04:45 +0530

    google tag installation

--- pages/_document.js
+++ pages/_document.js
@@ -10,29 +10,37 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/home3.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link 
-            href="https://fonts.googleapis.com/css2?family=Gentium+Basic:wght@400;700&display=swap" 
-            rel="stylesheet" 
+          <link
+            href="https://fonts.googleapis.com/css2?family=Gentium+Basic:wght@400;700&display=swap"
+            rel="stylesheet"
           />
-          <link 
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" 
-            rel="stylesheet" 
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
           />
 
-          {/* Google Tag (gtag.js) */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11405539991"></script>
+          {/* Google Tag removed - handled in _app.js */}
+          {/* Google Tag Manager */}
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'AW-11405539991');
-              `,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WPPP92S6');`,
             }}
           />
+          {/* End Google Tag Manager */}
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPPP92S6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
